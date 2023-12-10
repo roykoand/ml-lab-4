56468f6c4927f3c46fe00131c3e222f7651f9d70
roykoand <roykoand@gmail.com>
2023-12-10 19:33:29 +0200

    Linter

--- app/enums.py
+++ app/enums.py
@@ -1,7 +1,10 @@
 from enum import auto, Enum
 
+
 class StrEnum(str, Enum):
     pass
+
+
 class AutoName(StrEnum):
     def _generate_next_value_(name, start, count, last_values):
         return name
